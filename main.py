126473fdc8325117a6323fa48b3a92079bbec047
Atluri Arnav <120934@eesd.org>
2021-01-11 23:57:39 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello World,")
+
+print("My name is Arnav Atluri")
+print("I am in the Seventh Grade.")
+print("My favorite color is blue.")
+print(" My favorite sport is hockey.")
+print("My Favorite ice cream flavour is chocolate.")
